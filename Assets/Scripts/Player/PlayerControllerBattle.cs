a0dc3068aae5fa506e57c4133f7a2fdbfdb2e66c
Gal <gal.frias@gmail.com>
2023-07-07 17:47:41 -0600

    Player Controller in Battle

--- Assets/Scripts/Player/PlayerControllerBattle.cs
+++ Assets/Scripts/Player/PlayerControllerBattle.cs
@@ -5,6 +5,7 @@ public class PlayerControllerBattle : MonoBehaviour
     [SerializeField] float speed = 1;
     Rigidbody2D rgbd;
     Animator animator;
+    bool axisPressed;
 
     private void Start() {
         rgbd = GetComponent<Rigidbody2D>();
@@ -16,22 +17,34 @@ public class PlayerControllerBattle : MonoBehaviour
     }
 
     void movePlayer() {
+  
         float xMove;
         
         if(PlayerManager.instance.getPlayerState() == PlayerState.DeadState) {
             return;
         }
 
-        if(Input.GetAxisRaw("Horizontal") != 0) {
-            xMove = Input.GetAxisRaw("Horizontal") * speed;
+        xMove = Input.GetAxisRaw("Horizontal") * speed;
 
-            Vector3 movePos = new Vector3(xMove, 0, 0) * Time.deltaTime;
-
-            transform.position += movePos;
-
-            Debug.Log("horizontal value: " + xMove);
-            Debug.Log("position: " + transform.position);
+        if(xMove == 0) {
+            axisPressed = false;
         }
 
+        if(xMove < 0 && transform.position.x > -speed) {
+            if(axisPressed == false) {
+                axisPressed = true;
+                transform.position =  new Vector3 (transform.position.x + xMove, 0, 0);
+            }
+        }
+
+        if(xMove > 0 && transform.position.x < speed) {
+            if(axisPressed == false) {
+                axisPressed = true;
+                transform.position = new Vector3(transform.position.x + xMove, 0, 0);
+            }
+        }
+
+        Debug.Log("horizontal value: " + xMove);
+        Debug.Log("position: " + transform.position);
     }
 }
