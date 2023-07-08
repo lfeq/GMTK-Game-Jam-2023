e48ea048cea9e493586ba4b769dbd49643790589
Gal <gal.frias@gmail.com>
2023-07-07 20:10:13 -0600

    MoveCatchingBall script fixed and finished

--- Assets/Scripts/MoveCatchingBall.cs
+++ Assets/Scripts/MoveCatchingBall.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class MoveCatchingBall : MonoBehaviour
 {
     [SerializeField] float ballSpeed = 2f;
-    Transform playerPosition;
+    Vector2 playerPosition;
 
     private void Awake() {
-        playerPosition = PlayerControllerBattle.instance.transform;
-        Debug.Log("Player pos start: " + playerPosition.position);
+        playerPosition = PlayerControllerBattle.instance.transform.position;
+        Debug.Log("Player pos start: " + playerPosition);
     }
 
     private void FixedUpdate() {
-        transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, ballSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, playerPosition, ballSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, playerPosition) < 0.1f) {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
