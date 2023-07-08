9784637efb5c893b2d18b9ad03df2598e589ce53
lfeq <panchocosa@gmail.com>
2023-07-08 12:48:54 -0600

    Avances cambio de escena

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,12 +4,14 @@ public class LevelManager : MonoBehaviour {
     public static LevelManager s_instance;
 
     private LevelState m_levelState;
+    private Sprite enemySprite;
 
     private void Awake() {
         if (s_instance != null && s_instance != this) {
             Destroy(gameObject);
             return;
         }
+        DontDestroyOnLoad(gameObject);
         s_instance = this;
     }
 
@@ -24,8 +26,10 @@ public class LevelManager : MonoBehaviour {
         m_levelState = t_levelState;
         switch (m_levelState) {
             case LevelState.Escaping:
+                //Cambiar a escena de persecucion
                 break;
             case LevelState.Dodging:
+                //Cambiar a escena de atrapar
                 break;
             default:
                 throw new UnityException("Invalid Levela State");
@@ -38,6 +42,11 @@ public class LevelManager : MonoBehaviour {
 
     public void returnToMenu() {
         GameManager.s_instance.changeGameSate(GameState.LoadMainMenu);
+        Destroy(gameObject);
+    }
+
+    public void changeEnemySprite(Sprite t_enemySprite) {
+        enemySprite = t_enemySprite;
     }
 }
 
